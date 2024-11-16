2e47f9ab8cb1d573d3237b7641051f6ec947c97b
SumiyaRahman <sumiarahman01739@gmail.com>
2024-11-16 12:40:56 +0600

    context API

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import user from '../assets/user.png'
+import profile from '../assets/user.png'
+import { AuthContext } from '../provider/AuthProvider';
 
 const Navbar = () => {
+    const {user} = useContext(AuthContext);
     return (
         <div className='flex justify-between items-center'>
-            <div></div>
+            <div className='text-2xl font-semibold text-[#403F3F]'>{user && user.name}</div>
             <div className="nav-menu space-x-10 text-[#706F6F] text-base font-normal">
                 <Link to="/">Home</Link>
                 <Link to="/about">About</Link>
@@ -13,7 +15,7 @@ const Navbar = () => {
             </div>
             <div className="login flex items-center gap-3">
                 <div>
-                    <img className='w-9 h-9' src={user} />
+                    <img className='w-9 h-9' src={profile} />
                 </div>
                 <div>
                     <Link to='/auth/login' className='text-base font-semibold text-white py-2 px-10 bg-[#403F3F]'>Login</Link>
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import Router from './routes/Router'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import Router from "./routes/Router";
+import AuthProvider from "./provider/AuthProvider";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={Router}></RouterProvider>
-  </StrictMode>,
-)
+    <AuthProvider>
+      <RouterProvider router={Router}></RouterProvider>
+    </AuthProvider>
+  </StrictMode>
+);
